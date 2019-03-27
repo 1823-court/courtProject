9d08d5748ecd299be26396577922bfbd6de5a05c
LennieGao <1071830419@qq.com>
2019-03-27 14:59:19 +0800

    登陆注册

---
+++ src/components/registerOrLogin/customerService.vue
@@ -0,0 +1,109 @@
+<template>
+	<div class="drag">
+		<div class="header">
+			<div>
+				<div class="button" @click="goBack()">back</div>
+				<div class="title">{{title}}</div>
+			</div>
+			<div class="textbox">
+
+	
+				<!-- <p style="white-space: pre-line;">{{ message }}</p> -->
+				<br>
+				<textarea  placeholder="请输入您想要反馈的内容"></textarea><br>
+				<span style="font-size: 12px;">手机号 ：</span><input type="text" placeholder="请输入您的联系手机号">
+				<input type="submit" value="提交" class="register" /><br>
+				<!-- <Radio v-model="single">Radio</Radio> -->
+
+			</div>
+		<p class="footer" style="color:deeppink;">客服电话：400-321-4898</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '意见反馈',
+				single: false
+			}
+		},
+		methods: {
+			goBack() {
+				this.$router.go(-1);
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped="">
+	@import '../../common/css/index.less';
+
+	.drag {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		.w(375);
+		background: #F0F0F2;
+		font-size: 16px;
+
+		.header {
+			.w(375);
+			.h(50);
+			background: #AF6131;
+			color: #fff;
+			.l_h(50);
+			text-align: center;
+
+			.button {
+				position: absolute;
+				left: 20px;
+
+			}
+			.footer{
+				margin-top: 60px;
+			}
+		}
+
+
+		.textbox {
+			color: black;
+
+			.Court {
+				display: inline-block;
+			}
+
+			input {
+				.w(260);
+				.h(30);
+				margin-top: 20px;
+			}
+
+	
+
+			.register {
+				margin-top: 30px;
+				border: none;
+				.w(200);
+				.h(45);
+				background: #AF6131;
+				font-size: 16px;
+				color: #fff
+			}
+
+			input,
+			button {
+				border-radius: 6px;
+			}
+			textarea{
+				.w(320);
+				.h(282);
+			}
+		
+
+		}
+
+
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,78 +1,58 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
+// ------------------------------LennieGao--------------------------------------------
+import Register from "../components/registerOrLogin/register";
+import Login from "../components/registerOrLogin/login";
+import CustomerService from "../components/registerOrLogin/customerService";
+// ---register
+import RegisterJudge from "../components/registerOrLogin/home/register/registerJudge";
+import RegisterParty from "../components/registerOrLogin/home/register/registerParty";
+import RegisterAgreement from "../components/registerOrLogin/home/register/registerAgreement";
 
-import payPending from "../components/privyPage/home/payPending";
-import noticing from "../components/privyPage/home/noticing";
-import noticed from "../components/privyPage/home/noticed";
-
-import privyindex from "../components/privyPage/index";
-import privysearch from "../components/privyPage/search.vue";
-import privymine from "../components/privyPage/mine";
-
-import payPendingDetail from "../components/privyPage/home/detail/payPendingDetail";
-import noticingDetail from "../components/privyPage/home/detail/noticingDetail";
+//---login
+import ForgetPassword from "../components/registerOrLogin/home/login/forgetPassword";
 const router = new Router({
   routes: [
-    // 首页 搜索页 我的主页面路由
-    {
-      path: "/privyindex",
-      name: "privyindex",
-      component: privyindex,
-      // 首页里三个嵌套路由
-      children: [
-        {
-          path: "payPending",
-          name: "payPending",
-          component: payPending,
-          // 子路由嵌套路由
-          children: [
-            {
-              path: "payPendingDetail",
-              name: "payPendingDetail",
-              component: payPendingDetail
-            }
-          ]
-        },
-        {
-          path: "noticing",
-          name: "noticing",
-          component: noticing,
-          children: [
-            {
-              path: "noticingDetail",
-              name: "noticingDetail",
-              component: noticingDetail
-            }
-          ]
-        },
-        {
-          path: "noticed",
-          name: "noticed",
-          component: noticed
-        },
-        {
-          path: "/",
-          redirect: "/privyindex/payPending"
-        }
-      ]
-    },
-    {
-      path: "/privysearch",
-      name: "privysearch",
-      component: privysearch
-    },
-    {
-      path: "/privymine",
-      name: "privymine",
-      component: privymine
-    },
+		{
+		  path: "/register",
+		  name: "register",
+		  component: Register,
+		},
+		{
+		path: "/registerJudge",
+		name: "registerJudge",
+		component: RegisterJudge,
+		},
+			{
+		path: "/registerParty",
+		name: "registerParty",
+		component: RegisterParty,
+		},
+					{
+		path: "/registerAgreement",
+		name: "registerAgreement",
+		component: RegisterAgreement,
+		},
+		{
+		  path: "/login",
+		  name: "login",
+		  component: Login
+		},
+				{
+		path: "/forgetPassword",
+		name: "forgetPassword",
+		component: ForgetPassword,
+		},
+		{
+		  path: "/customerService",
+		  name: "customerService",
+		  component: CustomerService
+		},
     {
       path: "/",
-      redirect: "/privyindex"
+      redirect: "/register"
     }
   ]
 });
-
-
 export default router;
